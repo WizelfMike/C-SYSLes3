c4f6444eb975e0f6f8b0147d36d3498f52da9299
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-10-08 09:07:14 +0200

    Push for lesson 3 (Didn't push, my bad)

---
+++ EnemyAi/EnemyAi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EnemyAi
+{
+    class Program
+    {
+        
+        //ik ben helaas niet ver gekomen. Ik zal in m'n vrije tijd de theorie meer doornemen.
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Room
+    {
+        int[][] Tile = new int[2][];
+
+        public void Doit()
+        {
+            for (int y = 0; y < Tile.Length; y++)
+            {
+                Tile[y] = new int[5];
+            }
+        }
+    } 
+
+
+}
